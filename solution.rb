b35fa9583f825d5742b6766d08d27e6532aa4cc2
Your Name <you@example.com>
2017-08-18 11:29:28 -0500

    reto 6 sinatra plataforma ok :)

--- solution.rb
+++ solution.rb
@@ -1,10 +1,15 @@
 require 'sinatra'
 
 get '/' do
+	@num = 0
 	erb :form
 end
 
-post '/send-form' do
-	# "suma: #{params['contador'].to_i + 1}"
-
+get '/send-form' do
+	if params[:num]
+			@num = params[:num].to_i + 1
+		else
+			@num = 0
+		end
+		erb :form
 end
